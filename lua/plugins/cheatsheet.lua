5498014174d9c16f19ea886f1be00a3b18da5bbf
Sagging Rufus <saggingrufus@outlook.com>
2026-06-04 00:30:57 -0300

    adding cheatsheets

---
+++ lua/plugins/cheatsheet.lua
@@ -0,0 +1,9 @@
+return {
+    'sudormrfbin/cheatsheet.nvim', version = '*',
+    lazy = false,
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'nvim-lua/popup.nvim',
+      'nvim-lua/plenary.nvim',
+    }
+}
